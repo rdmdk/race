fd47f3816907312abd39fa875c5c887bdb138b82
Redmond <rdmdk@users.noreply.github.com>
2020-01-03 15:18:58 +0000

    Update race.js

--- race.js
+++ race.js
@@ -1,11 +1,9 @@
 document.body.style.height = window.innerHeight + "px";
 
-let runners = [];
+let runners = window.location.href.indexOf("#") > -1) ? window.location.href.replace(/.*?\#/gm, "") : localStorage.runners ? localStorage.runners : window.prompt("Enter a comma-delimitted list of runners' initials");
 if (window.location.href.indexOf("#") > -1) {
-	runners = window.location.href.replace(/.*?\#/gm, "");
 	setTimeout(() => window.location.href = window.location.href.replace(/\#.*?$/gm, ""), 100);
-} else if (localStorage.runners) runners = localStorage.runners.split(",");
-else runners = window.prompt("Enter a comma-delimitted list of runners' initials");
+}
 localStorage.runners = runners;
 
 let field = document.querySelector(".s1 ul");
