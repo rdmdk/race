9b662b5bccb32b6edfa6511c568b08af6f399dcb
Redmond <rdmdk@users.noreply.github.com>
2020-01-07 10:37:27 +0000

    Update race.js

--- race.js
+++ race.js
@@ -16,8 +16,8 @@ let ap = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", 
 let i = 0;
 runners = runners.toUpperCase().split(",").sort().sort((a, b) => a - b);
 runners.forEach((r) => {
-	let ii = i > ap.length ? 0 : i++;
-	field.insertAdjacentHTML("beforeend", "<li><span class='" + ap[ii] + "' title='Bench me'>" + r + "</span></li>");
+	i = i >= ap.length ? 0 : i++;
+	field.insertAdjacentHTML("beforeend", "<li><span class='" + ap[i] + "' title='Bench me'>" + r + "</span></li>");
 });
 
 let circles = document.querySelectorAll(".s1 span");
