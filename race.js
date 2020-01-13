68c9dff5de80741e6b4d4cd3532468cabcf3f452
Redmond <rdmdk@users.noreply.github.com>
2020-01-13 11:50:14 +0000

    Update race.js

--- race.js
+++ race.js
@@ -8,7 +8,7 @@ if (window.location.href.indexOf("#") > -1 && window.location.href.indexOf(",") 
 else {
 	let input = window.prompt("Enter a comma-delimitted list of runners' initials");
 	if (input !== null) {
-		if (input.match(/^[0-9]+/gm)) {
+		if (input.match(/^[0-9]+$/gm)) {
 			let t = parseInt(input);
 			for (let ti = 0; ti < t; ti++) {
 				runners += ti + ",";
