d3a2f4dff67f9903a30764eaf243c6d149d32636
Redmond <rdmdk@users.noreply.github.com>
2020-03-12 10:33:10 +0000

    Update race.js

--- race.js
+++ race.js
@@ -12,7 +12,7 @@ function newrunners(a) {
 	} else {
 		if (a === "abc") runners = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
 		else if (a === "periodic") runners = "h,he,li,be,b,c,n,o,f,ne,na,mg,al,si,p,s,cl,ar,k,ca,sc,ti,v,cr,mn,fe,co,ni,cu,zn,ga,ge,as,se,br,kr,rb,sr,y,zr,nb,mo,tc,ru,rh,pd,ag,cd,in,sn,sb,te,i,xe,cs,ba,la,ce,pr,nd,pm,sm,eu,gd,tb,dy,ho,er,tm,yb,lu,hf,ta,w,re,os,ir,pt,au,hg,tl,pb,bi,po,at,rn,fr,ra,ac,th,pa,u,np,pu,am,cm,bk,cf,es,fm,md,no,lr,rf,db,sg,bh,hs,mt,ds,rg,cn,nh,fl,mc,lv,ts,og";
-		else if (a === "ci") runners = "ac,alg,b,da,dl,l,m,mf,mt,o,p,r,sl,sw,v";
+		else if (a === "ci") runners = "ac,alg,b,da,dl,l,m,mf,mt,o,p,r,sl,sw,v"; //ac,b,da,dl,l,m,mf,mt,o,p,r,sw,v
 		else runners = a;
 	}
 	localStorage.runners = runners.replace(/^\,|\,$/gm, "").replace(/\,\,+/gm, ",");
